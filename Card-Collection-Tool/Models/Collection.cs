7b3542d27e4a98b941e567bda30dfd003a115a3c
vader9911 <vader9911@yahoo.com>
2024-08-29 20:45:25 -0400

    Add project files.

---
+++ Card-Collection-Tool/Models/Collection.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Card_Collection_Tool.Models
+{
+    public class Collection
+    {
+
+        public int id { get; set; }
+        public string Name { get; set; }
+
+        [Required]
+
+        public int? Description { get; set; }
+    }
+}
