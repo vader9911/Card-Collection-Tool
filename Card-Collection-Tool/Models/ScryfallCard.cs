4032a1c2f4e708a307d4dfc338b3528e9769526b
vader9911 <vader9911@yahoo.com>
2024-09-01 22:49:44 -0400

    update  scryfall card model for db. migrated new model to db

--- Card-Collection-Tool/Models/ScryfallCard.cs
+++ Card-Collection-Tool/Models/ScryfallCard.cs
@@ -1,31 +1,67 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
-namespace Card_Collection_Tool.Models
+public class ScryfallCard
 {
-    public class ScryfallCard
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("oracle_text")]
+    public string OracleText { get; set; }
+
+    [JsonPropertyName("mana_cost")]
+    public string ManaCost { get; set; }
+
+    [JsonPropertyName("set_name")]
+    public string SetName { get; set; }
+
+    [JsonPropertyName("type_line")]
+    public string TypeLine { get; set; }
+
+    [JsonPropertyName("scryfall_uri")]
+    public string? ScryfallUri { get; set; }
+
+    [JsonPropertyName("flavor_text")]
+    public string? FlavorText { get; set; }
+
+    [JsonPropertyName("artist")]
+    public string? Artist { get; set; }
+
+    [JsonPropertyName("rarity")]
+    public string? Rarity { get; set; }
+    [JsonPropertyName("keywords")]
+    public string? Keywords { get; set; }
+
+    [JsonPropertyName("image_uris")]
+    public ImageUris ImageUris { get; set; } // This will not directly map to a DB column
+
+    [JsonPropertyName("Prices")]
+    public Prices Prices { get; set; }
+}
+
+
+public class ImageUris
+{
+    [JsonPropertyName("small")]
+    public string? Small { get; set; }
+
+    [JsonPropertyName("normal")]
+    public string? Normal { get; set; }
+
+    [JsonPropertyName("large")]
+    public string? Large { get; set; }
+}
+
+public class Prices
     {
-        [JsonPropertyName("id")]
-        public string Id { get; set; }
+        [JsonPropertyName("usd")]
+        public string? USD { get; set; }
 
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
+        [JsonPropertyName("usd_foil")]
+        public string? USDFoil { get; set; }
 
-        [JsonPropertyName("set_name")]
-        public string SetName { get; set; }
-
-        [JsonPropertyName("image_uri")]
-        public string? ImageUri { get; set; }
-
-        [JsonPropertyName("oracle_text")]
-        public string? OracleText { get; set; }
-
-        [JsonPropertyName("mana_cost")]
-        public string? ManaCost { get; set; }
-
-        
-
-        [JsonPropertyName("type_line")]
-        public string? TypeLine { get; set; }
-    }
+        [JsonPropertyName("usd_etched")]
+        public string? USDEtched { get; set; }
 }
